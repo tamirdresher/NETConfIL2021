6da8fb112e6db9433f54bb9794a6e1534e4d2716
Tamir Dresher <tamirdr@payoneer.com>
2021-12-20 10:46:33 +0200

    new SPA templates

---
+++ WhatsNewInAspNetCore6/spa/weatherapp-react/src/setupProxy.js
@@ -0,0 +1,14 @@
+﻿const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const context = [
+    "/weatherforecast",
+];
+
+module.exports = function (app) {
+    const appProxy = createProxyMiddleware(context, {
+        target: 'http://localhost:5183',
+        secure: false
+    });
+
+    app.use(appProxy);
+};
